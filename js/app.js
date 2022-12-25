bbc148980967b81c1459e21dd642000fe33becfe
Maxdeoro <55924753+Maxdeoro@users.noreply.github.com>
2022-12-25 20:00:40 +0300

    adding to script

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 const msg = 'Hello World';
 console.log(msg);
 
+const bdy = document.querySelector('body');
+
 const btn = document.querySelector('button');
 btn.addEventListener('click', (e) => {
     btn.style.backgroundColor = 'green';
+});
+
+btn.addEventListener('onmouse', (e) => {
+    const myDiv = document.createElement('div');
+    myDiv.classList.add('my_div');
+    myDiv.style.width = '200px';
+    myDiv.style.height = '50px';
+    myDiv.innerHTML = 'CLICK ME, please!';
+    bdy.append(myDiv);
 });
